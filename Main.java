9e4295d5160752ec7191a03f57c9a86194df0e8f
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2023-02-25 18:33:31 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    int number1=30;
+    int number2=3;
+    //addition
+    System.out.println(number2+number1);
+    //modulor,or remainder
+    System.out.println(number1%number2);
+    //subtraction
+    System.out.println(number1-number2);
+    //multiplicaiton
+    System.out.println(number1*number2);
+    //division
+    System.out.println(number1/number2);
+  }
+}
+
